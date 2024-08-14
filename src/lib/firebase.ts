52b0c51d786e142b16b90cdfee4acf0c33f44031
Taka Horimoto <136284079+TakaHoriBighope@users.noreply.github.com>
2024-08-14 15:07:01 +0900

    deleted coments at firebase.ts(signIn, SignUp)

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -12,74 +12,10 @@ import {
   updateDoc,
 } from "firebase/firestore";
 
-// export const getInfos = async () => {
-//   const q = query(collection(db, "infos"), orderBy("createdAt", "desc"));
-
-//   const querySnapshot = await getDocs(q);
-//   const shops = querySnapshot.docs.map(
-//     (doc) => ({ ...doc.data(), id: doc.id } as Info)
-//   );
-//   return shops;
-// };
-
 export const updateUser = async (userId: string, params: any) => {
   const userRef = doc(db, "users", userId);
   await updateDoc(userRef, { params }); // {name: "Taka"}
 };
-
-// export const signin = async (email: string, password: string) => {
-//   // const auth = getAuth();
-//   const userCredential = await signInWithEmailAndPassword(
-//     auth,
-//     email,
-//     password
-//   );
-//   const { uid } = userCredential.user;
-//   const docRef = doc(db, "users", uid);
-//   const docSnap = await getDoc(docRef);
-//   if (docSnap.exists()) {
-//     return {
-//       id: uid,
-//       ...docSnap.data(),
-//     } as User;
-//   } else {
-//     console.log("No such document!");
-//     await setDoc(doc(db, "users", uid), {
-//       name: "",
-//       createdAt: Timestamp.fromDate(new Date()),
-//       updatedAt: Timestamp.fromDate(new Date()),
-//     });
-//     return {
-//       ...initialUser,
-//       id: uid,
-//     } as User;
-//   }
-// };
-
-// export const signup = async (
-//   email: string,
-//   password: string,
-//   username: string
-// ) => {
-//   const userCredential = await createUserWithEmailAndPassword(
-//     auth,
-//     email,
-//     password
-//   );
-//   const uid = userCredential.user.uid;
-//   await setDoc(doc(db, "users", uid), {
-//     email,
-//     coverPicture: "",
-//     profilePicture: "",
-//     followes: [],
-//     followings: [],
-//     createdAt: Timestamp.fromDate(new Date()),
-//     updatedAt: Timestamp.fromDate(new Date()),
-//     salesTalk: "",
-//     username,
-//     uid,
-//   });
-// };
 
 export const infoCreateRef = async (infoId: string) => {
   let collectionRef: CollectionReference<DocumentData> = collection(
